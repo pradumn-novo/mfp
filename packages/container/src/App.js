7b0ed5730ca5c4e24f2327fb321431376850b826
pradumn-novo <pradumn.bansal@novo.co>
2023-12-24 01:31:22 +0530

    feat: add header component to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => <><h1>Hi There!</h1><hr /><MarketingApp /></>
+const App = () => (<BrowserRouter>
+    <div>
+        <Header />
+        <MarketingApp />
+    </div>
+</BrowserRouter>)
 
 export default App
 
